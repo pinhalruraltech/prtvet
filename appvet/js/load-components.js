9de50932f71a1539b96df61ec0b1842a538c1f7d
pinhalruraltech <pinhalruraltech@gmail.com>
2026-03-13 15:55:20 -0300

    Update load-components.js

--- appvet/js/load-components.js
+++ appvet/js/load-components.js
@@ -1,1 +1,11 @@
+async function carregarMenu(){
 
+const resp = await fetch("../components/menu.html");
+
+const html = await resp.text();
+
+document.getElementById("menu").innerHTML = html;
+
+}
+
+carregarMenu();
