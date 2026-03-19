8cf9b5e736fae558258d1148dcd1fbbd00531804
pinhalruraltech <pinhalruraltech@gmail.com>
2026-03-19 20:28:55 -0300

    Criar menu.js

---
+++ menu/menu.js
@@ -0,0 +1,28 @@
+function toggleMenu(){
+    const menu = document.getElementById("menuHorizontal");
+    const icon = document.getElementById("menuIcon");
+
+    menu.classList.toggle("hidden");
+
+    if(menu.classList.contains("hidden")){
+        icon.setAttribute("data-lucide", "chevron-down");
+    } else {
+        icon.setAttribute("data-lucide", "chevron-up");
+    }
+
+    lucide.createIcons();
+}
+
+/* USUÁRIO */
+function carregarUsuario(){
+    const nome = "Venício"; // depois vem do Firebase
+    document.getElementById("usuarioNome").textContent = nome;
+}
+
+/* LOGOUT */
+function logout(){
+    alert("Logout");
+    // depois integrar com Firebase Auth
+}
+
+carregarUsuario();
