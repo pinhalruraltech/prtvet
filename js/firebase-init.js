2027064c201362e04ae8d7b24bb9bf94222ccc87
pinhalruraltech <pinhalruraltech@gmail.com>
2026-03-20 11:13:23 -0300

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,3 +1,4 @@
+// 🔥 IMPORTS FIREBASE
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
@@ -7,21 +8,26 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 
+// 🔐 CONFIG DO PROJETO (CORRIGIDO)
 const firebaseConfig = {
   apiKey: "AIzaSyDR69colU9JHvemBpab3x8PLHqaUm_5EM0",
   authDomain: "appvet-prt.firebaseapp.com",
   projectId: "appvet-prt",
   storageBucket: "appvet-prt.firebasestorage.app",
   messagingSenderId: "26046923372",
-  appId: "1:26046923372:web:fdbef180eadced87018e49"
+  appId: "1:26046923372:web:fdbe180eadced87018e49" // ✅ CORRETO
 };
 
 
+// 🚀 INICIALIZAÇÃO
 const app = initializeApp(firebaseConfig);
 
+
+// 🔗 SERVIÇOS
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
 
+// 📦 EXPORTAÇÃO PADRÃO DO SISTEMA
 export { auth, db, storage };
