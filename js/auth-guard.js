90b477d59f38f79bf355ed192f02cebf8a11534d
pinhalruraltech <pinhalruraltech@gmail.com>
2026-03-20 12:31:57 -0300

    Atualizar o auth-guard.js

--- js/auth-guard.js
+++ js/auth-guard.js
@@ -1,5 +1,18 @@
-const uid = localStorage.getItem("uid");
+import { auth } from "./firebase-init.js";
 
-if (!uid) {
-    window.location.href = "../index.html";
-}
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+        // 🔴 não logado
+        window.location.href = "../index.html";
+        return;
+    }
+
+    // ✅ logado — garante consistência
+    localStorage.setItem("uid", user.uid);
+
+});
